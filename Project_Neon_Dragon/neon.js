97c5b532373ecef96b90f87c2b908f68ccd62dc6
Minseo Kim <minseokim@Minseos-MBP-3.kornet>
2020-07-18 01:18:28 +0900

    Minseo's site

---
+++ Project_Neon_Dragon/neon.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click", function () {
+    document.getElementById("image").src="IMG_8075.JPG";
+  });
+
+document.getElementById("button2").addEventListener("click", function () {
+  alert("I have no idea");
+});
